8d2cab35b688468aff1d58aab098da98941c930d
wangzaixiang <949631531@qq.com>
2024-10-28 14:58:16 +0800

    compare filter/group-by/aggregate with simd version

--- poc/vector_example1/benches/my_benchmark.rs
+++ poc/vector_example1/benches/my_benchmark.rs
@@ -1,9 +1,9 @@
 #![feature(portable_simd)]
 
-use std::simd::u32x8;
+use std::simd::{u32x16, u32x8};
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use rand::random;
-use vector_example1::sort2::sort_u32x8;
+use vector_example1::sort2::{sort_u32x16, sort_u32x8};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
 
@@ -14,31 +14,51 @@ pub fn criterion_benchmark(c: &mut Criterion) {
 
 }
 
-fn simple_sort(vec: &mut Vec<u32>) {
-    vec.sort();
-}
+
 
 pub fn compare(c: &mut Criterion) {
-    let rand_nums: Vec<u32> = (0..8).map( |_ | random::<u32>() ).collect();
-    let mut vec2 = rand_nums.clone();
-    let mut vec = u32x8::from_slice(&rand_nums);
-
     let mut group = c.benchmark_group("sort");
-    group.bench_with_input(BenchmarkId::new("sort_u32x8", 1), &1,
-                            |b, i| b.iter(|| {
-                                vec = u32x8::from_slice(&rand_nums);
-                                vec[0] = *i as u32;
-                                sort_u32x8(black_box(&mut vec))
-                            } ));
+    {
+        let rand_nums: Vec<u32> = (0..8).map(|_| random::<u32>()).collect();
+        let mut vec2 = rand_nums.clone();
+        let mut vec = u32x8::from_slice(&rand_nums);
 
-    group.bench_with_input(BenchmarkId::new("simple",1), &1,
-                            |b, i| b.iter( || {
-                                vec2.clear();  vec2.extend_from_slice(&rand_nums);
-                                vec2[0] = *i as u32;
-                                simple_sort(black_box(&mut vec2))
-                            } ));
+        group.bench_with_input(BenchmarkId::new("simd sort u32x8", 1), &1,
+                               |b, i| b.iter(|| {
+                                   vec = u32x8::from_slice(&rand_nums);
+                                   vec[0] = *i as u32;
+                                   sort_u32x8(black_box(&mut vec))
+                               }));
 
-    println!("vec = {:?}, vec2 = {:?}", vec, vec2);
+        group.bench_with_input(BenchmarkId::new("simple sort u32x8", 1), &1,
+                               |b, i| b.iter(|| {
+                                   vec2.clear();
+                                   vec2.extend_from_slice(&rand_nums);
+                                   vec2[0] = *i as u32;
+                                   vec2.sort();
+                               } ));
+    }
+
+    {
+        let rand_nums: Vec<u32> = (0..16).map(|_| random::<u32>()).collect();
+        let mut vec2 = rand_nums.clone();
+        let mut vec = u32x16::from_slice(&rand_nums);
+
+        group.bench_with_input(BenchmarkId::new("simd sort u32x16", 1), &1,
+                               |b, i| b.iter(|| {
+                                   vec = u32x16::from_slice(&rand_nums);
+                                   vec[0] = *i as u32;
+                                   sort_u32x16(black_box(&mut vec))
+                               }));
+
+        group.bench_with_input(BenchmarkId::new("simple sort u32x16", 1), &1,
+                               |b, i| b.iter(|| {
+                                   vec2.clear();
+                                   vec2.extend_from_slice(&rand_nums);
+                                   vec2[0] = *i as u32;
+                                   vec2.sort();
+                               } ));
+    }
 
 }
 
--- poc/vector_example1/src/sort2.rs
+++ poc/vector_example1/src/sort2.rs
@@ -1,6 +1,6 @@
 use std::simd::cmp::SimdOrd;
-use std::simd::{simd_swizzle, u32x4, u32x8, ToBytes};
-
+use std::simd::{simd_swizzle, u32x16, u32x4, u32x8, ToBytes};
+use rand::random;
 
 #[test]
 fn test_sort_u32x8() {
@@ -12,6 +12,21 @@ fn test_sort_u32x8() {
     println!("nums = {:?}", nums)
 
 }
+
+#[test]
+fn test_sort_u32x16() {
+
+    for i in 0..256 {
+        let mut nums: Vec<u32> = (0..16).map( |_| random::<u32>() % 1000 ).collect();
+        let mut simd = u32x16::from_slice(&nums);
+        sort_u32x16(&mut simd);
+
+        nums.sort();
+        assert_eq!(simd.as_array() as &[u32], &nums);
+    }
+
+}
+
 
 
 /// sort u32x8 using simd
@@ -84,4 +99,116 @@ pub fn sort_u32x8(nums: &mut u32x8) {
     }
 
 }
+
+
+#[inline]
+pub fn sort_u32x16(nums: &mut u32x16) {
+    let a: u32x8 = u32x8::from_slice(&nums[0..8]);
+    let b: u32x8 = u32x8::from_slice(&nums[8..16]);
+
+    // sort 1v1 * 8 group
+    let min = a.simd_min(b);    // [ 8, 7, 6, 5, 4, 3, 2, 1]
+    let max = a.simd_max(b);    // [16,15,14,13,12,11,10, 9]
+
+    let a = simd_swizzle!(min, max, [0, 8, 1, 9, 2, 10, 3, 11]);  // [8, 16, 7, 15, 6, 14, 5, 13],
+    let b = simd_swizzle!(min, max, [4, 12, 5, 13, 6, 14, 7, 15]);  // b = [4, 12, 3, 11, 2, 10, 1, 9]
+    // end 1v1 * 8 group, now a, b is 2v2 * 4 group
+
+    let min: u32x8 = a.simd_min(b);    // [2,6,1,5]
+    let max: u32x8 = a.simd_max(b);    // [4,8,3,7]
+
+    let tmp = simd_swizzle!(min, [1, 0, 3, 2, 5, 4, 7, 6]);   // [12, 4, 11, 3, 19, 2, 9, 1]
+    let min = tmp.simd_min(max);  //    [8, 4, 7, 3, 6, 2, 5, 1]
+    let max = tmp.simd_max(max);  //     [12, 16, 11, 15, 10, 14, 9, 13]
+
+    let min = min.rotate_elements_left::<1>();  // [4, 7, 3, 6, 2, 5, 1, 8]
+    let a = simd_swizzle!(min, max, [0, 1, 8, 9, 2, 3, 10, 11]);   // [4, 7, 12, 16, 3, 6, 11, 15]
+    let b = simd_swizzle!(min, max, [4, 5, 12, 13, 6, 7, 14, 15]); //  [2, 5, 10, 14, 1, 8, 9, 13]
+    // end 2v2 * 4 grpup, now a:b is 4v4 * 2 group
+
+    let min = a.simd_min(b);
+    let max = a.simd_max(b);
+
+    // round 1
+    let tmp = simd_swizzle!(min, [1,2,3,0,5,6,7,4]);
+    let min = tmp.simd_min(max);
+    let max = tmp.simd_max(max);
+
+    // round 2
+    let tmp = simd_swizzle!(min, [1,2,3,0,5,6,7,4]);
+    let min = tmp.simd_min(max);
+    let max = tmp.simd_max(max);
+
+    // round 3
+    let tmp = simd_swizzle!(min, [1,2,3,0,5,6,7,4]);
+    let min = tmp.simd_min(max);
+    let max = tmp.simd_max(max);
+
+    // round 4
+    let min = simd_swizzle!(min, [1,2,3,0,5,6,7,4]);
+
+    let a = simd_swizzle!(min, max, [0,1,2,3, 8,9,10,11]);
+    let b = simd_swizzle!(min, max, [4,5,6,7, 12,13,14,15]);
+    // now a:b is 8v8 * 1 group
+
+    let min = a.simd_min(b);
+    let max = a.simd_max(b);
+
+    // round 1
+    let tmp = min.rotate_elements_left::<1>();
+    let min = tmp.simd_min(max);    // [2,4,6,1]
+    let max = tmp.simd_max(max);    // [3,5,7,8]
+
+    // round 2
+    let tmp = min.rotate_elements_left::<1>();
+    let min = tmp.simd_min(max);    // [3,5,1,2]
+    let max = tmp.simd_max(max);    // [4,6,7,8]
+
+    // round 3
+    let tmp = min.rotate_elements_left::<1>();
+    let min = tmp.simd_min(max);    // [4,1,2,3]
+    let max = tmp.simd_max(max);    // [5,6,7,8]
+
+    // round 4
+    let tmp = min.rotate_elements_left::<1>();
+    let min = tmp.simd_min(max);    // [4,1,2,3]
+    let max = tmp.simd_max(max);    // [5,6,7,8]
+
+    // round 5
+    let tmp = min.rotate_elements_left::<1>();
+    let min = tmp.simd_min(max);    // [4,1,2,3]
+    let max = tmp.simd_max(max);    // [5,6,7,8]
+
+    // round 6
+    let tmp = min.rotate_elements_left::<1>();
+    let min = tmp.simd_min(max);    // [4,1,2,3]
+    let max = tmp.simd_max(max);    // [5,6,7,8]
+
+    // round 7
+    let tmp = min.rotate_elements_left::<1>();
+    let min = tmp.simd_min(max);    // [4,1,2,3]
+    let max = tmp.simd_max(max);    // [5,6,7,8]
+
+    // round 8
+    let min = min.rotate_elements_left::<1>();
+
+    #[cfg(target_endian = "little")]
+    {
+        let p0: &mut u32x8 = unsafe { &mut *(&mut nums[0] as *mut u32 as *mut u32x8) };
+        let p1: &mut u32x8 = unsafe { &mut *(&mut nums[8] as *mut u32 as *mut u32x8) };
+
+        *p0 = min;
+        *p1 = max;
+    }
+
+    #[cfg(target_endian = "big")]
+    {
+        let p0: &mut u32x8 = unsafe { &mut *(&mut nums[0] as *mut u32 as *mut u32x8) };
+        let p1: &mut u32x8 = unsafe { &mut *(&mut nums[8] as *mut u32 as *mut u32x8) };
+
+        *p0 = max;
+        *p1 = min;
+    }
+}
+
 
