6b3d6f21db369e6bd0f22a95a7ba6dbaa3c2650e
wangzaixiang <949631531@qq.com>
2025-04-18 14:31:28 +0800

    arrow demo

--- playgrounds/try_datafusion/src/main.rs
+++ playgrounds/try_datafusion/src/main.rs
@@ -1,42 +1,87 @@
+use clap::Parser;
 use datafusion::functions_aggregate::sum::sum;
-use datafusion::logical_expr::LogicalPlanBuilder;
 use datafusion::prelude::*;
-use datafusion::prelude::Partitioning::RoundRobinBatch;
+
+#[derive(Parser, Debug)]
+#[command(name = "datafusion playground", version = "0.1", author = "wangzx")]
+struct Arguments {
+
+    /// SQL for "select * from csv('users.csv') where age >= 30"
+    #[arg(long)]
+    sql1: bool,
+
+    /// dataframe for "select * from csv('users.csv') where age >= 30"
+    #[arg(long)]
+    dataframe1: bool,
+
+    /// sql for "select * from access_log l left join users u on l.user = u.name"
+    #[arg(long)]
+    sql2: bool,
+
+    /// case45 via sql, large join
+    #[arg(long)]
+    case45sql: bool,
+
+    /// case45 via dataframe, large join
+    #[arg(long)]
+    case45df: bool,
+
+    /// case45 in right-assoc order
+    #[arg(long)]
+    case45_df2: bool,
+}
 
 #[tokio::main]
 async fn main() -> datafusion::error::Result<()>{
-    test_sql2().await?;
-    // test_case45_via_sql().await?;
-    // test_case45_via_dataframe().await?;
-    // test_case45_via_dataframe2().await?;
+    let args = Arguments::parse();
+
+    if args.sql1 {
+        test_sql1().await?;
+    }
+    if args.dataframe1 {
+        test_dataframe1().await?;
+    }
+    if args.sql2 {
+        test_sql2().await?;
+    }
+    if args.case45sql {
+        test_case45_via_sql().await?;
+    }
+    if args.case45df {
+        test_case45_via_dataframe().await?;
+    }
+    if args.case45_df2 {
+        test_case45_via_dataframe2().await?;
+    }
     Ok(())
 }
 
 
-#[tokio::test]
 async fn test_sql1() -> datafusion::error::Result<()> {
     let ctx = SessionContext::new();
 
-    ctx.register_csv("users", "data/users.csv", CsvReadOptions::new()).await?;
+    ctx.register_csv("users", "playgrounds/try_datafusion/data/users.csv", CsvReadOptions::new()).await?;
     let df = ctx.sql("SELECT * FROM users where age >= 30").await?;
 
+    try_explain(&df).await?;
     df.show().await?;
 
     Ok(())
 }
 
-#[tokio::test]
 async fn test_dataframe1() -> datafusion::error::Result<()> {
     use datafusion::functions_aggregate::count::count;
     let ctx = SessionContext::new();
 
-    let table = ctx.read_csv("data/users.csv", CsvReadOptions::new()).await?;
+    let table = ctx.read_csv("playgrounds/try_datafusion/data/users.csv", CsvReadOptions::new()).await?;
 
     let result = table.filter( col("age").gt_eq(lit(20)) )?
         .aggregate(vec![col("sex")], 
                    vec![count(col("sex")), sum(col("age"))])?
         .sort_by(vec![col("sex")])?;
-    result.clone().explain(true, true)?.show().await?;
+
+    try_explain(&result).await?;
+
     result.show().await?;
 
     Ok(())
@@ -60,10 +105,11 @@ async fn prepare_dataset(ctx: &SessionContext) -> datafusion::error::Result<()> 
 }
 
 async fn test_case45_via_sql() -> datafusion::error::Result<()> {
-    let ctx = SessionContext::new();
+    // let ctx = SessionContext::new();
+    let config = SessionConfig::new().with_repartition_joins(false); //
+    let ctx = SessionContext::new_with_config(config);
     prepare_dataset(&ctx).await?;
 
-    let time0 = std::time::Instant::now();
     let df = ctx.sql("select wt.tag_name, sum(wt.amount) from (
 	select si.sale_item_id as sale_item_id, 
 		si.sale_order_id as sale_order_id,
@@ -84,17 +130,35 @@ async fn test_case45_via_sql() -> datafusion::error::Result<()> {
      left join tags t on t.tag_id = ct.tag_id 
 ) as wt where wt.tag_name = 'tag1' group by wt.tag_name").await?;
 
+
+    try_explain(&df).await?;
+
     let tm0 = std::time::Instant::now();
-    df.clone().explain(false, true)?.show().await?;
+    df.show().await?;
     let tm1 = std::time::Instant::now();
-    println!("explain time: {:?}", tm1.duration_since(tm0));
-    // df.show().await?;
-    let time1 = std::time::Instant::now();
-    println!("test_case45_via_sql time: {:?}", time1.duration_since(time0));
+    println!("test_case45_via_sql time: {:?}", tm1.duration_since(tm0));
 
     Ok(())
 }
 
+/// if the EXPLAIN environment variable is set to "analyze|verbose", it will show the execution plan with execution time
+async fn try_explain(df: &DataFrame) -> datafusion::error::Result<()> {
+        match std::env::var("EXPLAIN") {
+            Ok(val) if val == "analyze" => {
+                df.clone().explain(false, true)?.show().await?;
+            },
+            Ok(val) if val == "verbose" => {
+                df.clone().explain(true, false)?.show().await?;
+            },
+            Ok(_) => {
+                df.clone().explain(false, false)?.show().await?;
+            },
+            _ => {}
+        }
+    Ok(())
+}
+
+// use this method to debug the hash join source code
 async fn test_sql2() -> datafusion::error::Result<()> {
     let config = SessionConfig::new().with_repartition_joins(false);
 
@@ -103,44 +167,35 @@ async fn test_sql2() -> datafusion::error::Result<()> {
 
     // create external table users stored as csv location 'playgrounds/try_datafusion/data/users.csv';
     // create external table access_log stored as csv location 'playgrounds/try_datafusion/data/access_log.csv';
-    let users = ctx.read_csv("playgrounds/try_datafusion/data/users.csv", CsvReadOptions::new()).await?
-        .repartition(RoundRobinBatch(1))?;
-    let access_log = ctx.read_csv("playgrounds/try_datafusion/data/access_log.csv", CsvReadOptions::new()).await?
-        .repartition(RoundRobinBatch(1))?;
+    let _users = ctx.read_csv("playgrounds/try_datafusion/data/users.csv", CsvReadOptions::new()).await?;
+    let _access_log = ctx.read_csv("playgrounds/try_datafusion/data/access_log.csv", CsvReadOptions::new()).await?;
 
-    // ctx.register_table("users", users.into_view())?;
-    // ctx.register_table("access_log", access_log.into_view())?;
-
-    let time0 = std::time::Instant::now();
     // let df = ctx.sql("select s.order_date, sum(si.amount) from sale_items si
     //  left join sale_orders s on si.sale_order_id = s.sale_order_id group by s.order_date limit 10").await?;
 
-    // let df = ctx.sql("select * from access_log l left join users u on l.`user` = u.name").await?;
-    let df = access_log.join(users, JoinType::Left, &["user"], &["name"], None)?;
+    let df = ctx.sql("select * from access_log l left join users u on l.`user` = u.name").await?;
+    // let df = access_log.join(users, JoinType::Left, &["user"], &["name"], None)?;
+    try_explain(&df).await?;
 
-    df.clone().explain(true, false)?.show().await?;
-    // let time1 = std::time::Instant::now();
-    let time1 = time0.clone();
-
+    let time0 = std::time::Instant::now();
     df.show().await?;
-    let time2 = std::time::Instant::now();
-    println!("test_sql2 explain {:?}, execute: {:?}", time1.duration_since(time0), time2.duration_since(time1));
+    let time1 = std::time::Instant::now();
+    println!("test_sql2 execute: {:?}", time1.duration_since(time0));
 
     Ok(())
 }
 
 async fn test_case45_via_dataframe() -> datafusion::error::Result<()> {
     let ctx = SessionContext::new();
-    
     let root = format!("{}/workspaces/wangzaixiang/mpp_test/datafusion", std::env::var("HOME").unwrap());
-    
+
     // 注册所有表
     let sale_items = ctx.read_parquet(format!("{root}/sale_items.parquet"), ParquetReadOptions::default()).await?.alias("si")?;
     let sale_orders = ctx.read_parquet(format!("{root}/sale_orders.parquet"), ParquetReadOptions::default()).await?.alias("so")?;
     let customers = ctx.read_parquet(format!("{root}/customers.parquet"), ParquetReadOptions::default()).await?.alias("c")?;
     let customer_tags = ctx.read_parquet(format!("{root}/customer_tags.parquet"), ParquetReadOptions::default()).await?.alias("ct")?;
     let tags = ctx.read_parquet(format!("{root}/tags.parquet"), ParquetReadOptions::default()).await?.alias("t")?;
-    
+
     // 构建查询
     let time0 = std::time::Instant::now();
     
@@ -198,6 +253,7 @@ async fn test_case45_via_dataframe() -> datafusion::error::Result<()> {
         )?;
 
     // result.clone().explain(true, false)?.show().await?;
+    try_explain(&result).await?;
     
     // 显示结果
     result.show().await?;
@@ -280,7 +336,7 @@ async fn test_case45_via_dataframe2() -> datafusion::error::Result<()> {
             vec![sum(col("amount"))],
         )?;
 
-    // result.clone().explain(false, true)?.show().await?;
+    try_explain(&result).await?;
 
     // 显示结果
     result.show().await?;
