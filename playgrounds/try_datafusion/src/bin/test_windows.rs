7115618573229227530e2860ae1af718ca399a7c
wangzaixiang <949631531@qq.com>
2025-05-18 21:32:56 +0800

    test code for window function

--- playgrounds/try_datafusion/src/bin/test_windows.rs
+++ playgrounds/try_datafusion/src/bin/test_windows.rs
@@ -1,30 +1,95 @@
 use std::sync::Arc;
+use chrono::NaiveDate;
 use datafusion::arrow::array::{Date32Array, Float32Array, Float64Array, Int32Array, Int64Array, RecordBatch, UInt32Array, UInt64Array};
 use datafusion::arrow::datatypes::{DataType, Field, Schema};
-use datafusion::error::Result;
+use datafusion::error::{DataFusionError, Result};
 use datafusion::prelude::{ParquetReadOptions, SessionConfig, SessionContext};
 use datafusion_datasource::memory::MemorySourceConfig;
 use datafusion_datasource::source::DataSourceExec;
 use rand::RngCore;
 
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main(){
+
+}
+
+/// WindowAggExec + PlainAggregateWindowExpr + Accumulator
+#[tokio::test]
+async fn test_sum_1() -> Result<()> {
     let mut ctx = SessionContext::new();
     _ = prepare(&mut ctx).await?;
 
-    let df2 = ctx.sql("select *, \n\
-    sum(amount) over (partition by product_id) as \"product_amounts\", -- WindowAggExec \n\
-    sum(amount) over (partition by product_id order by order_date rows 1 preceding) as \"amounts1\", -- BoundedWindowAggExec \n\
-    rank() over (partition by product_id order by order_date) as rank1, -- BoundedWindowAggExec \n\
-    rank() over (partition by product_id order by order_date desc) as rank2 -- BoundedWindowAggExec \n\
-      from t1").await?;
-
-    df2.clone().explain(false, true)?.show().await?;
-
-    _ = df2.show().await?;
-
+    let df2 = ctx.sql("select *, sum(amount) over (partition by product_id) as product_amounts from t1").await?;
+    df2.show().await?;
     Ok(())
 }
+
+/// case 2: BoundedWindowAggExec + SlidingAggregateWindowExpr + Accumulator(sliding)
+#[tokio::test]
+async fn test_sum_2() -> Result<()> {
+    let mut ctx = SessionContext::new();
+    _ = prepare(&mut ctx).await?;
+
+    let df2 = ctx.sql("select *, \
+    sum(amount) over (partition by product_id order by order_date rows 1 preceding) as amounts1 from t1").await?;
+
+    _ = df2.show().await;
+    Ok(())
+}
+
+/// BoundedWindowAggExec + PlainAggregateWindowExpr + Accumulator
+#[tokio::test]
+async fn test_sum_3() -> Result<()> {
+    let mut ctx = SessionContext::new();
+    _ = prepare(&mut ctx).await?;
+
+    let df2 = ctx.sql("select *, \
+    sum(amount) over (partition by product_id order by order_date rows between unbounded preceding and 1 following) as amounts1 from t1").await?;
+
+    _ = df2.show().await;
+    Ok(())
+}
+
+
+/// case 3: BoundedWindowAggExec + StandardWindowExpr + PartitionEvaluator::evaluate
+#[tokio::test]
+async fn test_rank_1() -> Result<()> {
+    let mut ctx = SessionContext::new();
+    _ = prepare(&mut ctx).await?;
+
+    let df2 = ctx.sql("select *, rank() over (partition by product_id order by order_date) as rank1 from t1").await?;
+    _ = df2.show().await;
+    Ok(())
+}
+
+/// ! frame is defined but not used
+#[tokio::test]
+async fn test_rank_2() -> Result<()> {
+    let mut ctx = SessionContext::new();
+    _ = prepare(&mut ctx).await?;
+
+    let df2 = ctx.sql("select *, rank() over (partition by product_id order by order_date rows 1 preceding) as rank1 from t1").await?;
+    _ = df2.show().await;
+    Ok(())
+}
+
+/// WindowAggExec + StandardWindowExpr + PartitionEvaluator::evaluate_all
+#[tokio::test]
+async fn test_ntile_1() -> Result<()> {
+    let mut ctx = SessionContext::new();
+    _ = prepare(&mut ctx).await?;
+
+    let df2 = ctx.sql("select *, ntile(4) over () as ntile from t1").await?;
+    _ = df2.show().await?;
+    Ok(())
+}
+
+fn parse_date(str: &str) -> Result<i32> {
+    let date = NaiveDate::parse_from_str(str, "%Y-%m-%d")
+        .map_err(|e| DataFusionError::Execution(e.to_string()))?;
+    let days = date.signed_duration_since(NaiveDate::from_ymd(1970,1,1)).num_days();
+    Ok(days as i32)
+}
+
 
 async fn prepare(ctx: &mut SessionContext) -> Result<()> {
     // build a memory dataframe with columns: product_id, order_date, quantity, amount
@@ -39,12 +104,18 @@ async fn prepare(ctx: &mut SessionContext) -> Result<()> {
 
     let mut rng = rand::thread_rng();
 
-    let range = 0..30;
+    let range = 0..10;
     let id_array: Vec<u64> = range.clone().map(|i| i).collect();
-    let product_id_array: Vec<u32> = range.clone().map( |_| rng.next_u32() % 10 ).collect();
-    let order_date_array: Vec<i32> = range.clone().map( |_| (rng.next_u32() % 365) as i32 ).collect();
-    let amount_array: Vec<f64> = range.clone().map( |_| (rng.next_u32() + 1000) as f64 % 10000f64 / 100.0 ).collect();
-    let quantity_array: Vec<u32> = range.clone().map( |_| (rng.next_u32() + 1) % 5 ).collect();
+    let product_id_array: Vec<u32> = vec![1, 2, 3, 4, 2, 3, 3, 4, 4, 4];
+    let order_date_array: Vec<i32> = vec![ parse_date("2025-01-01")?, parse_date("2025-01-01")?,
+                                           parse_date("2025-01-01")?, parse_date("2025-01-01")?,
+                                           parse_date("2025-01-02")?, parse_date("2025-01-02")?,
+                                           parse_date("2025-01-02")?, parse_date("2025-01-03")?,
+                                           parse_date("2025-01-03")?, parse_date("2025-01-04")?,
+    ];
+
+    let quantity_array: Vec<u32> = vec![2,1,3,4,1,2,2,3,2,1];
+    let amount_array: Vec<f64> = vec![15.0, 20.0, 25.0, 30.0, 18.0, 38.0, 25.0, 13.0, 87.0 ,67.0];
 
     let batch = RecordBatch::try_new(
         schema.clone(),
